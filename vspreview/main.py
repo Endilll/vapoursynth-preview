899b820f1d12ff0aac71847c1b47d004379fa2de
Endill <brainvlad@gmail.com>
2019-05-23 14:58:42 +0300

    Main: go back to CPU rendering
    Since it's better in every aspect for vspreview.

--- vspreview/main.py
+++ vspreview/main.py
@@ -294,7 +294,7 @@ class MainWindow(AbstractMainWindow):
     FPS_AVERAGING_WINDOW_SIZE  = FrameInterval(100)
     FPS_REFRESH_INTERVAL       =   150  # ms
     LOG_LEVEL          = logging.DEBUG
-    OPENGL_RENDERING           =  True
+    OPENGL_RENDERING           = False
     OUTPUT_INDEX               =     0
     PLAY_BUFFER_SIZE = FrameInterval(5)
     PNG_COMPRESSION_LEVEL      =    80  # 0 - 100
