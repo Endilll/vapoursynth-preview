c155af6207c021311ad90718f127493cf28ba2da
SFNozomi <35213529+SaltyChiang@users.noreply.github.com>
2021-05-27 23:53:16 +0800

    Update abstracts.py

--- vspreview/core/abstracts.py
+++ vspreview/core/abstracts.py
@@ -142,9 +142,8 @@ class AbstractToolbar(Qt.QWidget, QABC):
                 'Storage loading: Toolbar: failed to parse toggle')
             toggle = self.main.TOGGLE_TOOLBAR
 
-        if not self.toggle_button.isChecked():
-            if toggle:
-                self.toggle_button.click()
+        if self.toggle_button.isChecked() != toggle:
+            self.toggle_button.click()
 
 
 class AbstractToolbars(AbstractYAMLObjectSingleton):
